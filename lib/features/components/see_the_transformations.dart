801d49d5b934738aebea5c9b0320ede3e7807c05
fiki2002 <adepitanoluwatosin202@gmail.com>
2023-06-21 10:09:57 +0100

    feat: see transformation done

--- lib/features/components/see_the_transformations.dart
+++ lib/features/components/see_the_transformations.dart
@@ -8,30 +8,45 @@ class SeeTheTransformations extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          AppStrings.seeTheTransformations,
-          style: AppTypography.baseStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w700,
+    final data = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: data.width * 0.06,
+      ),
+      child: Column(
+        children: [
+          Text(
+            AppStrings.seeTheTransformations,
+            style: AppTypography.baseStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+            ),
           ),
-        ),
-        Common.gap(height: 16),
-        Text(
-          AppStrings.checkOutText,
-          textAlign: TextAlign.center,
-          style: AppTypography.baseStyle(),
-        ),
-        Common.gap(height: 50),
-        Row(
-          children: [
-            Image.asset(AppStrings.image1),
-            Image.asset(AppStrings.image2),
-            Image.asset(AppStrings.image3),
-          ],
-        )
-      ],
+          Common.gap(height: 16),
+          Text(
+            AppStrings.checkOutText,
+            textAlign: TextAlign.center,
+            style: AppTypography.baseStyle(),
+          ),
+          Common.gap(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                AppStrings.image1,
+              ),
+              Image.asset(
+                AppStrings.image3,
+              ),
+              Image.asset(
+                AppStrings.image2,
+              ),
+            ],
+          ),
+          Common.gap(height: 50),
+        ],
+      ),
     );
   }
 }
